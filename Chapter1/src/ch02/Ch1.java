5e97b9d44006cc2a85eb2d409a1ee67067b3c175
JoonGyu Park <system6805@naver.com>
2022-06-14 20:51:28 +0900

    #업로드 연습

---
+++ Chapter1/src/ch02/Ch1.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Ch1 {
+	public static void main(String[] args) {
+		int age, count;
+		age = 10;
+		
+		int level = 10000;
+		System.out.println(age);
+		System.out.println(level);
+
+	}
+
+}
---
+++ Chapter1/src/ch06/VariableTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class VariableTest {
+	public static void main(String[] args) {
+		
+		byte bnum = 127;
+//		-128~127까지 표현이 가능함
+		
+		System.out.println(bnum);
+		
+//		int num = 12379909990;
+		long lNum = 12379909990L;
+		System.out.println(lNum);
+	}
+
+}
